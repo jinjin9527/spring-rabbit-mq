7c406785d99b1b6f5c1035e1ed142617646cd4e9
sylinx_zj <sylinx_zj@163.com>
2021-01-09 21:08:39 +0900

    Initial commit

---
+++ spring-rabbit-mq-01-helloword/src/main/java/com/sylinx/config/Tut1Config.java
@@ -0,0 +1,30 @@
+package com.sylinx.config;
+
+import com.sylinx.receiver.Tut1Receiver;
+import com.sylinx.sender.Tut1Sender;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"tut1","hello-world"})
+@Configuration
+public class Tut1Config {
+
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public Tut1Receiver receiver() {
+        return new Tut1Receiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut1Sender sender() {
+        return new Tut1Sender();
+    }
+}
